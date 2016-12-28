a10f12b8e2b7f535bc312c9dae864bc8ec504bf4
S405-PC22\user <123>
2016-12-28 18:49:18 +0800

    bg

--- MyApplication6/app/src/main/java/com/example/kimber/myapplication/MainActivity.java
+++ MyApplication6/app/src/main/java/com/example/kimber/myapplication/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.kimber.myapplication;
 
 import android.content.Context;
 import android.content.SharedPreferences;
+import android.graphics.drawable.Drawable;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
